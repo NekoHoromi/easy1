c746099fc3ac210ada9439aedac8074e7e710805
Хороми <128912298+NekoHoromi@users.noreply.github.com>
2024-03-04 14:58:20 +0300

    Add files via upload

---
+++ easy3.cpp
@@ -0,0 +1,46 @@
+#include "easy.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int itc_sqrt(int num){
+    if (num == 0){
+        return 0;
+    }
+    if (num < 0){
+        return -1;
+    }
+    long long kv = 1;
+    while (kv * kv < num){
+            kv++;
+    }
+    if (kv * kv == num){
+        return kv;
+    }
+    return -1;
+}
+int itc_skv(int num){
+    if (num <= 0){
+        return -1;
+    }
+    return num * num;
+}
+int itc_spr(int a, int b){
+    if (a <= 0 || b <= 0){
+        return -1;
+    }
+    return a * b;
+}
+double itc_str(int a, int b, int c){
+    if (a <= 0 || b <= 0 || c <= 0){
+        return -1;
+    }
+    double p = (a + b + c) / 2;
+    return sqrt(p * ((p - a) * (p - b) * (p - c)));
+
+}
+double itc_scir(int radius){
+
+    return 3.14 * (radius * radius);
+}
