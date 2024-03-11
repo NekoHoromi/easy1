31afb3647003700a9e6e62909361a9f4b800f5ff
Хороми <128912298+NekoHoromi@users.noreply.github.com>
2024-03-11 13:38:30 +0300

    Update easy4.cpp

--- easy4.cpp
+++ easy4.cpp
@@ -30,12 +30,16 @@ bool itc_ispositive(int num){
     if (num == 0 || num > 0){
         return true;
     }
+    else{
     return false;
+    }
 
 }
 bool itc_ispositive_d(double num){
     if (num == 0 || num > 0){
         return true;
     }
+    else{
     return false;
+    }
 }
