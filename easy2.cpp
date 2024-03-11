fd97e38a4cf2c12a1aab9c299bee29293b1129d0
Хороми <128912298+NekoHoromi@users.noreply.github.com>
2024-03-11 13:37:05 +0300

    Update easy2.cpp

--- easy2.cpp
+++ easy2.cpp
@@ -7,13 +7,17 @@ bool itc_iseven(int num){
     if (num % 2 == 0){
         return true;
     }
+    else{
     return false;
+    }
 }
 int itc_max(int num, int num2){
     if (num > num2){
         return num;
     }
+    else{
     return num2;
+    }
 }
 int itc_min(int min1, int min2){
     if (min1 < min2){
@@ -25,11 +29,15 @@ double itc_fmax(double num, double num2){
     if (num > num2){
         return num;
     }
+    else{
     return num2;
+    }
 }
 double itc_fmin(double num, double num2){
     if (num < num2){
         return num;
     }
+    else{
     return num2;
+    }
 }
